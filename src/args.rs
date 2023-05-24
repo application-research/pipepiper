512abff57ed6a8c8ad5710ccb1d073cf130ce635
brkp <brkp@tutanota.com>
2023-05-24 17:16:51 +0300

    argument parsing

---
+++ src/args.rs
@@ -0,0 +1,48 @@
+use std::net::SocketAddr;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PiperMode {
+    Server,
+    Client,
+}
+
+argwerk::define! {
+    #[usage = "ppr [-h] [recv | send <ip> <port>]"]
+    pub struct Args {
+        pub help: bool = false,
+        #[required = "run mode"]
+        pub mode: PiperMode,
+        #[required = "run mode"]
+        pub addr: SocketAddr,
+    }
+    /// Print the help text.
+    ["-h" | "--help"] => {
+        help = true;
+    }
+    /// Run piper in client mode.
+    ["recv", ip, port] => {
+        mode = Some(PiperMode::Server);
+        addr = Some(format!("{ip}:{port}").parse::<SocketAddr>()?);
+    }
+    /// Run piper in server mode.
+    ["send", ip, port] => {
+        mode = Some(PiperMode::Client);
+        addr = Some(format!("{ip}:{port}").parse::<SocketAddr>()?);
+    }
+}
+
+pub fn parse_args() -> Args {
+    match Args::args() {
+        Ok(args) => {
+            if args.help {
+                std::process::exit(1);
+            }
+            args
+        }
+        Err(why) => {
+            eprintln!("{}", Args::help());
+            eprintln!("{why:#}");
+            std::process::exit(1);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(warnings)]
+
+mod args;
+
+#[tokio::main]
+async fn main() {
+    let args = args::parse_args();
+
+    match args.mode {
+        args::PiperMode::Server => todo!(),
+        args::PiperMode::Client => todo!(),
+    };
 }
